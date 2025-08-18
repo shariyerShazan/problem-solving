5dff2105aa785fef98dc5acbe4ba88b0bef71a1f
shariyerShazan <shariyershazan1@gmail.com>
2025-08-18 15:18:33 +0600

    2nd problem add

--- problem.js
+++ problem.js
@@ -1,8 +1,25 @@
+
+// problem one
 const reverseString= (string)=>{
     let reverse = ""
     for(let i = (string.length-1) ; i>= 0 ; i--){
         reverse += string[i]
     }
-    console.log(`${string} it's reverse is: `,reverse)
+    console.log(`"${string}" it's reverse is: ${reverse}`)
 }
 reverseString("shazan")
+
+
+// problem two 
+const countVowel = (string)=>{
+        let count = 0
+       const vowel = "aeiou"
+       for(let i = 0 ; i< string.length ; i ++){
+           if(vowel.includes(string[i].toLocaleLowerCase())){
+            count++
+           }
+       }
+       console.log(`"${string}": total vowel is ${count}`)
+}
+countVowel("shazanA")
+
