52e4e5a22d7ba75fa1e499605783fdf4ce216393
shariyerShazan <shariyershazan1@gmail.com>
2025-09-16 18:04:39 +0600

    string and number get with fget()

---
+++ C/DAY_1/arr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>  
+
+int main() {
+    int id;
+    printf("Enter id = ");
+    scanf("%d", &id);
+    getchar(); 
+
+    char a[10];
+    printf("Enter string = ");
+    fgets(a, 10, stdin);
+
+    a[strlen(a) - 1] = '\0';
+
+    printf("ID = %d and Name = %s\n", id, a);
+    return 0;
+}
---
+++ Python/hold.py
@@ -0,0 +1,7 @@
+Price_list = [
+    "01", "Banana", 45.00, "unit",
+    "02", "Mango", 80.00, "kg",
+    "03", "Apple", 55.00, "kg",
+    "04", "Papaya", 65.00, "unit",
+    "05", "Guava", 35.00, "kg"
+]
