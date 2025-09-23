e1397e9060c7698d8452e2f2d47124457e9a49af
shariyerShazan <shariyershazan1@gmail.com>
2025-09-24 04:49:51 +0600

    diamond

---
+++ C/pattern/four.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main(){
+
+    int n , row , col ;
+    printf("Enter a number =");
+    scanf("%d" , &n);
+
+//  right - up tringle
+        for(row = 1 ; row <= n ; row++){
+               for(col = 1 ; col <= n-row ; col++){
+                printf("  ");
+               }
+               for(col = 1 ; col <= row ; col++){
+                printf("* ");
+               }
+               printf("\n");
+        }
+  printf("\n");
+
+  
+
+
+
+        // dimond
+        for(row = 1 ; row <= n ; row++){
+               for(col = 1 ; col <= n-row ; col++){
+                printf("  ");
+               }
+               for(col = 1 ; col <= row ; col++){
+                printf("* ");
+               }
+               for(col = row -1 ; col >=1 ; col--){
+                printf("* ");
+               }
+               printf("\n");
+          }
+          for(row = n-1 ; row >= 1 ; row--){
+               for(col = 1 ; col <= n-row ; col++){
+                printf("  ");
+               }
+               for(col = 1 ; col <= row ; col++){
+                printf("* ");
+               }
+               for(col = row -1 ; col >=1 ; col--){
+                printf("* ");
+               }
+               printf("\n");
+          }
+    return 0 ;
+}
