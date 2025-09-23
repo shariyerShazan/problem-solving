6b8d2c61b6bf4afe4468a53d32e87f8bf4640cc2
shariyerShazan <shariyershazan1@gmail.com>
2025-09-24 04:25:03 +0600

    reverse triangle

--- C/pattern/two.c
+++ C/pattern/two.c
@@ -2,7 +2,90 @@
 int main(){
 
    
+   int n  , row , col ;
+   printf("Enter N = ");
+   scanf("%d" , &n);
 
+//    row reverse
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("%d " , row);
+       }
+       printf("\n");
+   }
+printf("\n");
+
+//    row reverse odd / even
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("%d " , row % 2);
+       }
+       printf("\n");
+   }
+printf("\n");
+
+
+//    col reverse
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("%d " , col);
+       }
+       printf("\n");
+   }
+printf("\n");
+
+
+// col reverse odd / even
+ for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("%d " , col % 2);
+       }
+       printf("\n");
+   }
+printf("\n");
+
+
+
+//    row reverse ABC
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("%c " , row + 65);
+       }
+       printf("\n");
+   }
+printf("\n");
+
+
+
+//    col reverse ABC
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("%c " , col + 65);
+       }
+       printf("\n");
+   }
+printf("\n");
+
+
+// reverse star
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("* ");
+       }
+       printf("\n");
+   }
+printf("\n");
+
+
+
+// reverse hash
+   for(int row = n ; row >= 1 ; row-- ){
+       for(col = row ; col >= 1 ; col--){
+           printf("# ");
+       }
+       printf("\n");
+   }
+printf("\n");
 
     return 0 ;
 }
